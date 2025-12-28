ab12cbfe32e51af6388d483ef8e3319ec587286b
Tautvydas <server.profas@gmail.com>
2025-12-28 07:36:39 +0200

    Modify User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property int $role_id
+ * @property bool $is_active
+ * @property string $password
+ */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -21,6 +32,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'is_active',
     ];
 
     /**
@@ -44,5 +57,56 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function ownedProjects(): HasMany
+    {
+        return $this->hasMany(Project::class, 'owner_id');
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'project_members', 'user_id', 'project_id')
+            ->withPivot(['project_role', 'joined_at']);
+    }
+
+    public function reportedBugs(): HasMany
+    {
+        return $this->hasMany(Bug::class, 'reporter_id');
+    }
+
+    public function assignedBugs(): HasMany
+    {
+        return $this->hasMany(Bug::class, 'assigned_to');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'user_id');
+    }
+
+    public function uploadedAttachments(): HasMany
+    {
+        return $this->hasMany(Attachment::class, 'uploaded_by');
+    }
+
+    public function bugEvents(): HasMany
+    {
+        return $this->hasMany(BugEvent::class, 'actor_id');
+    }
+
+    public function apiTokens(): HasMany
+    {
+        return $this->hasMany(ApiToken::class, 'user_id');
+    }
+
+    public function watchedBugs(): BelongsToMany
+    {
+        return $this->belongsToMany(Bug::class, 'bug_watchers', 'user_id', 'bug_id');
     }
 }
