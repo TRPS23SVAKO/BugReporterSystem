e0271bb3b17c0b20f7154484086b72963bda544d
Tautvydas <server.profas@gmail.com>
2025-12-30 00:18:18 +0200

    Remove value limit

--- database/migrations/2025_12_28_034912_create_settings_table.php
+++ database/migrations/2025_12_28_034912_create_settings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('key', 100)->unique();
-            $table->string('value', 255);
+            $table->string('value');
             $table->text('description')->nullable();
             $table->timestamps();
         });
