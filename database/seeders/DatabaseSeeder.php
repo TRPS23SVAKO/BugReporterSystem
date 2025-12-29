a741c33f801fc32f95c97b74a98e4831a1d7a557
Tautvydas <server.profas@gmail.com>
2025-12-30 00:18:05 +0200

    Add default roles and settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Sukuriame rolės
+        Role::query()->updateOrCreate([
+            'id' => 0,
+            'name' => 'user',
+            'display_name' => 'Vartotojas',
+            'role_color' => '6b6b6b' // Pilka
+        ]);
+        Role::query()->updateOrCreate([
+            'id' => 1,
+            'name' => 'admin',
+            'display_name' => 'Administratorius',
+            'role_color' => '4326ff' // Mėlyna
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Create default settings
+        Setting::query()->updateOrCreate([
+            'id' => 0,
+            'key' => 'default_role',
+            'value' => 'user',
+            'description' => 'Pradinė rolė, kurią gaus prisijungias nauajs vartotojas.'
+        ]);
+        Setting::query()->updateOrCreate([
+            'id' => 1,
+            'key' => 'main_page_desc_image',
+            'value' => 'Logo.jpeg',
+            'description' => 'Pagrindinio puslapio aprašymo paveikslas'
+        ]);
+        Setting::query()->updateOrCreate([
+            'id' => 2,
+            'key' => 'main_page_desc_text',
+            'value' => 'Klaidų valdymo sistema – tai internetinė aplikacija, skirta projektų ir programinių klaidų registravimui, stebėjimui bei tvarkymui. Vartotojai gali kurti projektus, priskirti klaidas, sekti jų būsenas, prioritetus ir sprendimo eigą, taip užtikrinant aiškų bendradarbiavimą ir efektyvų darbų valdymą.',
+            'description' => 'Pagrindinio puslapio aprašymas'
         ]);
     }
 }
