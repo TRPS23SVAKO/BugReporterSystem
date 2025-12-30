8de5077d45f8a1f5105915dc1ca8e7ec4997f3a4
Tautvydas <server.profas@gmail.com>
2025-12-30 02:11:54 +0200

    Add bug statuses routes

--- routes/admin.php
+++ routes/admin.php
@@ -5,8 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
-Route::patch('/form/settings/update/{setting}', [DashboardController::class, 'updateSetting'])->name('admin.web.settings.update');
+Route::prefix('forms')->group(function () {
+    Route::patch('/settings/update/{setting}', [DashboardController::class, 'updateSetting'])->name('admin.web.settings.update');
 
-Route::post('/form/roles/store', [DashboardController::class, 'storeRole'])->name('admin.web.roles.store');
-Route::patch('/form/roles/update/{role}', [DashboardController::class, 'updateRole'])->name('admin.web.roles.update');
-Route::delete('/form/roles/delete/{role}', [DashboardController::class, 'deleteRole'])->name('admin.web.roles.delete');
+    Route::post('/roles/store', [DashboardController::class, 'storeRole'])->name('admin.web.roles.store');
+    Route::patch('/roles/update/{role}', [DashboardController::class, 'updateRole'])->name('admin.web.roles.update');
+    Route::delete('/roles/delete/{role}', [DashboardController::class, 'deleteRole'])->name('admin.web.roles.delete');
+
+    Route::post('/bug-statuses', [DashboardController::class, 'storeBugStatus'])->name('admin.web.bug-statuses.store');
+    Route::patch('/bug-statuses/{bugStatus}', [DashboardController::class, 'updateBugStatus'])->name('admin.web.bug-statuses.update');
+    Route::delete('/bug-statuses/{bugStatus}', [DashboardController::class, 'destroyBugStatus'])->name('admin.web.bug-statuses.destroy');
+});
